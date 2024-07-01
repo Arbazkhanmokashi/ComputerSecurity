f474f8fc984496465ee80ec579aeed808916c328
sinhasahil <sahil2472002@gmail.com>
2024-07-01 16:48:23 +0100

    added two public classes for sending message and connected users

--- ChatApplication/ChatApplication.API/Hub/ChatHub.cs
+++ ChatApplication/ChatApplication.API/Hub/ChatHub.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.SignalR;
+
 namespace ChatApplication.API.Hub;
 
 public class ChatHub : Microsoft.AspNetCore.SignalR.Hub {
@@ -14,6 +16,21 @@ public class ChatHub : Microsoft.AspNetCore.SignalR.Hub {
         await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room!);
         _connection[Context.ConnectionId] = userConnection;
         await Clients.Group(userConnection.Room!)
-        .SendAsync(method:"ReceiveMessage", "Lets Program Bot", $"{userConnection.User} has Joined the Group");
+        .SendAsync(method:"ReceiveMessage", arg1: "Lets Program Bot", arg2: $"{userConnection.User} has Joined the Group");
+    }
+
+    public async Task SendMessage(string message) {
+        if(_connection.TryGetValue(Context.ConnectionId, out UserRoomConnection userRoomConnection)){
+            await Clients.Group(userRoomConnection.Room!)
+            .SendAsync(method: "ReceiveMessage", arg1: userRoomConnection.User, arg2: message, arg3: DateTime.Now);
+        }
+    }
+
+    public Task SendConnectedUser(string room)
+    {
+        var users = _connection.Values
+        .Where(u => u.Room == room)
+        .Select(s => s.User);
+        return Clients.Group(room).SendAsync(method:"ConnectedUser", users); 
     }
 }
