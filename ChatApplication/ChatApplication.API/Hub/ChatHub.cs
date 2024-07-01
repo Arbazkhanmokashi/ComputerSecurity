b829166b6ea7b57887ef9191306147686d499a54
sinhasahil <sahil2472002@gmail.com>
2024-07-01 14:59:55 +0100

    added user connection , public task of joining the room, and outputs

--- ChatApplication/ChatApplication.API/Hub/ChatHub.cs
+++ ChatApplication/ChatApplication.API/Hub/ChatHub.cs
@@ -2,8 +2,18 @@ namespace ChatApplication.API.Hub;
 
 public class ChatHub : Microsoft.AspNetCore.SignalR.Hub {
 
+    private readonly IDictionary<string, UserRoomConnection> _connection;
+
+    public ChatHub(IDictionary<string, UserRoomConnection> connection)
+    {
+        _connection = connection;
+    }
+
     public async Task JoinRoom(UserRoomConnection userConnection)
     {
         await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room!);
+        _connection[Context.ConnectionId] = userConnection;
+        await Clients.Group(userConnection.Room!)
+        .SendAsync(method:"ReceiveMessage", "Lets Program Bot", $"{userConnection.User} has Joined the Group");
     }
 }
