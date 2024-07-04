caab18bf755c7cd54f6362373f6d9e6e29fa4ebf
Arbazkhanmokashi <161367954+Arbazkhanmokashi@users.noreply.github.com>
2024-07-04 18:24:52 +0100

    updated file

--- ChatApplication/ChatApplication.API/Hub/ChatHub.cs
+++ ChatApplication/ChatApplication.API/Hub/ChatHub.cs
@@ -4,3 +4,7 @@ using Microsoft.AspNetCore.SignalR;
 namespace ChatApplicationYT.Hub;
 public class ChatHub : Microsoft.AspNetCore.SignalR.Hub
 {
+       private readonly IDictionary<string, UserRoomConnection> _connection;
+
+    public ChatHub(IDictionary<string, UserRoomConnection> connection)
+    {
