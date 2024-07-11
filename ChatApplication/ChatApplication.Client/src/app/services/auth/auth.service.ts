34a88497c4896c646b36dc56fb05fd0958c2b298
AkshayN01 <nambly77@gmail.com>
2024-07-12 00:57:35 +0100

    Updated auth service to add google auth logic

--- ChatApplication/ChatApplication.Client/src/app/services/auth/auth.service.ts
+++ ChatApplication/ChatApplication.Client/src/app/services/auth/auth.service.ts
@@ -1,20 +1,68 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import { LoginResponse } from '../../models/responses/login-response';
 import { ApiService } from '../http/api.service';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
+import firebase from 'firebase/compat/app';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+declare const google: any;
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private googleApiUrl = environment.googleLoginurl;
+  private sessionStorageName = "user-data";
+  private userSubject = new BehaviorSubject<firebase.User | null>(null);
+  public user$ = this.userSubject.asObservable();
+  public userTest : any = null;
 
-  constructor(private apiservice: ApiService) { }
+  constructor(private afAuth: AngularFireAuth, private ngZone: NgZone) {
+    this.initializeGoogleOneTap();
+    this.afAuth.authState.subscribe(user => {
+      // this.userTest = user;
+      this.userSubject.next(user);
+    });
+  }
 
-  GoogleLogin(credential: string): Observable<LoginResponse>{
+  private initializeGoogleOneTap() {
+    window.onload = () => {
+      google.accounts.id.initialize({
+        client_id: environment.googleClientId,
+        callback: this.handleCredentialResponse.bind(this)
+      });
+       // @ts-ignore
+       google.accounts.id.renderButton(
+        // @ts-ignore
+        document.getElementById("googleBtn"),
+          { theme: "outline", size: "large", width: "100%"}
+      );
 
-    return this.apiservice.postData<LoginResponse, string>(this.googleApiUrl, JSON.stringify(credential));
+      google.accounts.id.prompt();
+    };
+  }
+
+  private async handleCredentialResponse(response: any) {
+    const credential = firebase.auth.GoogleAuthProvider.credential(response.credential);
+    await this.afAuth.signInWithCredential(credential).then(() => {
+      this.ngZone.run(() => {
+        console.log('User signed in with Google One Tap');
+        // const user = this.userSubject.getValue();
+        // sessionStorage.setItem(this.sessionStorageName, JSON.stringify(user));
+      });
+    }).catch((error) => {
+      console.error('Error signing in with Google One Tap', error);
+    });
+  }
+
+  getUserEmail(): string | null {
+    const user = this.userSubject.getValue();
+    return user ? user.email : null;
+  }
+
+  isLoggedIn(): boolean {
+    console.log(this.userTest);
+    return this.userTest !== null;
   }
 }
