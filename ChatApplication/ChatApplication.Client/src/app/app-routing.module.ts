4a27dde4b357870bbcfc48041233d8c7781d2f23
AkshayN01 <nambly77@gmail.com>
2024-07-12 01:00:38 +0100

    added routes

--- ChatApplication/ChatApplication.Client/src/app/app-routing.module.ts
+++ ChatApplication/ChatApplication.Client/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { IsAuthGuard } from './oauth.guard';
+import { HomeComponent } from './components/protected/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent,
+    canActivate: [IsAuthGuard],}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
