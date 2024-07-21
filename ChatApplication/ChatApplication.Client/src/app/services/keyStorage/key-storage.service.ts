f5b13fccdc84482578d153b6787103f1f5f59db5
Arbazkhanmokashi <161367954+Arbazkhanmokashi@users.noreply.github.com>
2024-07-21 23:43:20 +0100

    Updated KeyStorage service functions

--- ChatApplication/ChatApplication.Client/src/app/services/keyStorage/key-storage.service.ts
+++ ChatApplication/ChatApplication.Client/src/app/services/keyStorage/key-storage.service.ts
@@ -1,18 +1,43 @@
 import { Injectable } from '@angular/core';
+import * as CryptoJS from 'crypto-js';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class KeyStorageService {
   private privateKeyStorageName = "user-key";
+  private deviceNumberStorageName = "device";
+  private secretKey = environment.secretKey;
 
   constructor() { }
 
   storeKey = (key:string) =>{
-    localStorage.setItem(this.privateKeyStorageName, key);
+    const val = window.btoa(key)
+    const ciphertext = CryptoJS.AES.encrypt(val, this.secretKey).toString();
+    localStorage.setItem(this.privateKeyStorageName, ciphertext);
   }
 
   retrieveKey = () => {
-    return localStorage.getItem(this.privateKeyStorageName);
+    const encryptedValue = localStorage.getItem(this.privateKeyStorageName);
+    if (encryptedValue) {
+      try {
+        const bytes = CryptoJS.AES.decrypt(encryptedValue, this.secretKey);
+        var val = bytes.toString(CryptoJS.enc.Utf8);
+        return window.atob(val);
+      } catch (e) {
+        console.error('Error decrypting local storage item', e);
+        return null;
+      }
+    }
+    return null;
+  }
+
+  storeDeviceNumber = (key:string) =>{
+    localStorage.setItem(this.deviceNumberStorageName, key);
+  }
+
+  retrieveDeviceNumber = () => {
+    return localStorage.getItem(this.deviceNumberStorageName);
   }
 }
