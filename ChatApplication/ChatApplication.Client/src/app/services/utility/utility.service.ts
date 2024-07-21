e719aea7874d3d766413e9aea5933cb478a1a035
AkshayN01 <nambly77@gmail.com>
2024-07-21 23:37:58 +0100

    Added services to setup app after login

---
+++ ChatApplication/ChatApplication.Client/src/app/services/utility/utility.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilityService {
+
+  constructor() { }
+
+  getUniqueGUID = () : string => {
+    function s4(): string {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return (
+      s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4()
+    );
+  }
+}
